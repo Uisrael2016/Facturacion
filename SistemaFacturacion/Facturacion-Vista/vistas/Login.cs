cc96494a701aebea7c59683cd665e346862ea3bb
blackbs1994 <andres.ph_94@hotmail.es>
2016-05-13 14:05:17 -0500

    andres 13

--- SistemaFacturacion/Facturacion-Vista/vistas/Login.cs
+++ SistemaFacturacion/Facturacion-Vista/vistas/Login.cs
@@ -28,7 +28,7 @@ namespace Facturacion_Vista
                UsuarioDao UsuarioDao = new UsuarioDao();
                bool de = UsuarioDao.validaUsuario("eea");
                 MessageBox.Show("Cambio realizado", "CAmbio hecho por mi");
-                // esto es un mensaje
+                // esto es un mensaje andres 13 
                 // esto es mensaje
             }
             catch(Exception ex)
