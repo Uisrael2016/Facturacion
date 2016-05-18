dad17844fac6566e60243daeeba03a5743ded52a
javy1989 <javier.tec1989@gmail.com>
2016-05-18 17:05:38 -0500

    subida de devcomponent

--- SistemaFacturacion/Facturacion-Vista/vistas/Login.Designer.cs
+++ SistemaFacturacion/Facturacion-Vista/vistas/Login.Designer.cs
@@ -28,55 +28,20 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.textUser = new System.Windows.Forms.TextBox();
-            this.textPassword = new System.Windows.Forms.TextBox();
-            this.btnIngresar = new System.Windows.Forms.Button();
             this.SuspendLayout();
-            // 
-            // textUser
-            // 
-            this.textUser.Location = new System.Drawing.Point(190, 82);
-            this.textUser.Name = "textUser";
-            this.textUser.Size = new System.Drawing.Size(135, 20);
-            this.textUser.TabIndex = 0;
-            // 
-            // textPassword
-            // 
-            this.textPassword.Location = new System.Drawing.Point(190, 126);
-            this.textPassword.Name = "textPassword";
-            this.textPassword.Size = new System.Drawing.Size(135, 20);
-            this.textPassword.TabIndex = 1;
-            // 
-            // btnIngresar
-            // 
-            this.btnIngresar.Image = global::Facturacion_Vista.Properties.Resources.home_ico;
-            this.btnIngresar.Location = new System.Drawing.Point(115, 176);
-            this.btnIngresar.Name = "btnIngresar";
-            this.btnIngresar.Size = new System.Drawing.Size(85, 40);
-            this.btnIngresar.TabIndex = 2;
-            this.btnIngresar.UseVisualStyleBackColor = true;
-            this.btnIngresar.Click += new System.EventHandler(this.btnIngresar_Click);
             // 
             // Login
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(472, 274);
-            this.Controls.Add(this.btnIngresar);
-            this.Controls.Add(this.textPassword);
-            this.Controls.Add(this.textUser);
             this.Name = "Login";
             this.Text = "Form1";
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.TextBox textUser;
-        private System.Windows.Forms.TextBox textPassword;
-        private System.Windows.Forms.Button btnIngresar;
     }
 }
 
